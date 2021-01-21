b9f5582f782bcbd976d70f6530b48dbe8881b897
swathibk <swathibk95@gmail.com>
2021-01-21 17:31:17 +0530

    icons,contact us,navbar

--- intern-action.php
+++ intern-action.php
@@ -1,42 +1,58 @@
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <title>DHARWAD HUBBALLI TUTOR</title>
-    <link rel="icon" href="media/img/favicon.png" type="image/png" sizes="96x96">
-    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <title>Dharwadhubballitutor</title>
+    <link rel="icon" href="media/img/favicon.png" type="image/png" sizes="96x96" />
+    <meta name="title" content="DharwadHubballiTutor" />
+    <meta name="description" content="DharwadHubballiTutor provides Trainings on various courses and helps upgrade Skills so as to make everyone Job ready" />
+    <meta name="keywords" content="HTML,CSS,Java,Python,Digital Marketing,Revit,Autocad,Cloud Computing,Android,Graphic designing" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <meta charset="utf-8" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam:wght@100&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam:wght@100&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="css/dharwadhubballitutor.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="css/dharwadhubballitutor.css">
+    <link href="https://allfont.net/allfont.css?fonts=league-spartan" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet" />
   </head>
   <body>
-    <section class="top-header">
-     <div class="brand-class container-fluid">
+    <div class="brand-class container-fluid">
       <div class="row">
         <div class="col-md-6">
-          <img src="media/img/logo.png"> </img>
-            <a class="navbar-brand" href="#">DharwadHubballiTutor</a>
+          <!-- <img src="media/img/logo.png" class="logoimg" alt="DharwadHubballiTutor logo" /> -->
+          <a class="navbar-brand brandName" href="#">DharwadHubballiTutor</a>
+          <br/>
+        
+          <a class="social-icon fbicon" target="_blank" href="https://www.facebook.com/DharwadhubballiTutor"><i class="fab fa-facebook fa-lg fbicon fbheader"></i></a>
+          <a class="social-icon twittericon" target="_blank" href="https://twitter.com/DharwadTutor"><i class="fab fa-twitter fa-lg twittericon twitterheader"></i></a>
+          <a class="social-icon instaicon" target="_blank" href="https://www.instagram.com/dharwadhubballitutor/"><i class="fab fa-instagram fa-lg instaicon instaheader"></i></a>
+          <a class="social-icon instaicon" target="_blank" href="http://www.linkedin.com/in/dharwadhubballitutor"><i class="fab fa-linkedin fa-lg linkedinicon linkedinheader"></i></a>
+          <a class="social-icon whatsappicon" target="_blank" href="https://api.whatsapp.com/send?phone=917204817334&amp;text=Hi there! I have a question :)"><i class="fab fa-whatsapp fa-lg whatsappicon whatsappheader"></i></a>
+         
         </div>
         <div class="col-md-6">
           <div class="semicircle">
-            <a class="blink_me" href="#"> Job Assistance</a>
+            <a class="blink_me" href="Job-Assistance.html"> Job Assistance</a>
           </div>
-            
+          <button type="button" class="mobileshow">Job Assistance</button>
         </div>
       </div>
-     </div>
-    </section>
-  </br>
-      <nav class="navbar container-fluid navbar-expand-sm bg-light">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fa fa-bars" style="color:black"></i>
+    </div>
+
+    <br />
+    <nav class="navbar navbar-expand-md bg-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="true" aria-label="Toggle navigation">
+        <i class="fa fa-bars"></i>
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav mx-auto">
+        <ul class="navbar-nav mx-auto">
           <li class="nav-item active">
             <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
           </li>
@@ -49,18 +65,18 @@
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Trainings </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="Webdesigning.html">Web Designing and Development</a>
-              <a class="dropdown-item" href="Python.html">Python Programming </a>
-              <a class="dropdown-item" href="Cloudcomputing.html">Cloud Computing</a>
-              <a class="dropdown-item" href="Androiddevelop.html">Android Development</a>
-              <a class="dropdown-item" href="Civildesign.html">Civil Design Softwares</a>
-              <a class="dropdown-item" href="Programlang.html">Programming Languages</a>
-              <a class="dropdown-item" href="Digitalmarketing.html">Digital Marketing</a>
-              <a class="dropdown-item" href="SchoolAcad.html">School Academics</a>
+              <a class="dropdown-item" href="Web-Designing-and-Development-Training.html">Web Designing and Development</a>
+              <a class="dropdown-item" href="Python-Programming-Training.html">Python Programming </a>
+              <a class="dropdown-item" href="Cloud-Computing-Training.html">Cloud Computing</a>
+              <a class="dropdown-item" href="Android-Development-Training.html">Android Development</a>
+              <a class="dropdown-item" href="Civil-Design-Softwares-Training.html">Civil Design Softwares</a>
+              <a class="dropdown-item" href="Programming-Languages-Training.html">Programming Languages</a>
+              <a class="dropdown-item" href="Digital-Marketing-Training.html">Digital Marketing</a>
+              <a class="dropdown-item" href="School-Academics-Training.html">School Academics</a>
             </div>
-            <li class="nav-item">
-              <a class="nav-link" href="Internship.html">Internships</a>
-            </li>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Internship.html">Internships</a>
           </li>
         </ul>
         <ul class="navbar-nav">
